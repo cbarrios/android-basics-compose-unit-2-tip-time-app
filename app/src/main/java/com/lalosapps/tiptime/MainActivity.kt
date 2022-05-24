e9e201087d918edea754eba2f90eba47c2ca3595
cbarrios <lalobarrios16@yahoo.com>
2022-05-24 12:47:46 -0400

    master: (7) Use remember function to save state

--- app/src/main/java/com/lalosapps/tiptime/MainActivity.kt
+++ app/src/main/java/com/lalosapps/tiptime/MainActivity.kt
@@ -10,7 +10,10 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -59,10 +62,10 @@ fun TipTimeScreen() {
 
 @Composable
 fun EditNumberField() {
-    var amountInput = mutableStateOf("0")
+    var amountInput by remember { mutableStateOf("") }
     TextField(
-        value = amountInput.value,
-        onValueChange = { amountInput.value = it },
+        value = amountInput,
+        onValueChange = { amountInput = it },
     )
 }
 
