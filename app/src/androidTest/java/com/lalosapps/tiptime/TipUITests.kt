578df521e69b9a6c888bed2db7e7efb680da6cb5
cbarrios <lalobarrios16@yahoo.com>
2022-05-24 22:12:18 -0400

    testing: (5) Write an instrumentation test

---
+++ app/src/androidTest/java/com/lalosapps/tiptime/TipUITests.kt
@@ -0,0 +1,24 @@
+package com.lalosapps.tiptime
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import org.junit.Rule
+import org.junit.Test
+
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            MyTipTimeApp()
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)").performTextInput("20")
+        composeTestRule.onNodeWithText("Tip amount: $2.00").assertExists()
+    }
+}
